f0a055216ec2d6e3e5dc80a283c162fdd60a07f9
reactcodes <scripttagchannel@gmail.com>
2022-10-30 13:32:44 +0530

    homepage basic design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SearchContainerComponent } from './search-container/search-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent,
+    SearchContainerComponent
   ],
   imports: [
     BrowserModule,
